90f6853b9abbb8fa28cb51cff3276e0e5f48495b
lottiger <linda.lottiger@gmail.com>
2024-04-25 12:38:08 +0200

    fixed phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,17 @@ import { TfiEmail } from 'react-icons/tfi'
 export const Footer = () => {
   return (
     <div className='text-stone-200 bg-stone-500 text-sm h-10 mt-10 flex items-center justify-center'>
-    <div className='mx-5 flex justify-between w-full'> 
-    <div>
-          <a href="mailto:linda.lottiger@gmail.com" className='md:block hidden'>Email: linda.lottiger@gmail.com</a> {/* Gör e-postadressen klickbar här */}
-          <TfiEmail className='md:hidden block h-6 w-6 text-stone-200' /> {/* Visa ikonen på mindre skärmar */}
+      <div className='mx-5 flex justify-between w-full'> 
+        <div>
+          <a href="mailto:linda.lottiger@gmail.com" className='md:block hidden'>Email: linda.lottiger@gmail.com</a> 
+          <a href="mailto:linda.lottiger@gmail.com" className='md:hidden block'><TfiEmail className='h-6 w-6 text-stone-200' /></a>
         </div>
         <div>
           <p className=''>LINDA LÖTTIGER</p>
         </div>
         <div>
-          <a href="tel:+46768256943" className='md:block hidden'>Phone: +46{' '}76{' '}825{' '}6943</a> {/* Gör telefonnumret klickbart här */}
-          <FiPhoneCall className='md:hidden block h-6 w-6 text-stone-200' /> {/* Visa ikonen på mindre skärmar */}
+          <a href="tel:+46768256943" className='md:block hidden'>Phone: +46{' '}76{' '}825{' '}69{' '}43</a>
+          <a href="tel:+46768256943" className='md:hidden block'><FiPhoneCall className='h-6 w-6 text-stone-200' /></a>
         </div>
       </div>
     </div>
