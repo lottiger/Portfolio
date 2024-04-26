21587af1fa5dd587780e7e6a3451a60c10e80ef7
lottiger <linda.lottiger@gmail.com>
2024-04-26 13:36:11 +0200

    l

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -11,7 +11,7 @@ export const MessageForm = () => {
   };
 
   return (
-    <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
+    <form name="contact" method="POST" data-netlify="true" acction="/success" onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
       <label for="email" className='mt-5'>Email:</label>
       <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='rounded bg-stone-900 p-1 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }} />
       <label for="message" >Message:</label>
