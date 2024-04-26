7c1ef1ad05036c46555546639167a02e80a58fcf
lottiger <linda.lottiger@gmail.com>
2024-04-26 13:14:07 +0200

    m

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -11,11 +11,10 @@ export const MessageForm = () => {
   };
 
   return (
-    <form name="contact" method="POST" data-netlify="true"onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
-       <input type="hidden" name="form-name" value="contact" />
-      <label className='mt-5'>Email:</label>
+    <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
+      <label for="email" className='mt-5'>Email:</label>
       <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='rounded bg-stone-900 p-1 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }} />
-      <label>Message:</label>
+      <label for="message" >Message:</label>
       <textarea  name="message" id='message' value={message} rows={8} cols={24} onChange={(e) => setMessage(e.target.value)} required className='rounded bg-stone-900 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }}/>
       <button type="submit" value="Submit" className='bg-yellow-800 text-stone-200 rounded mt-3 py-1 px-6 mb-5 hover:bg-yellow-700 transition-colors'>Send</button>
     </form>
