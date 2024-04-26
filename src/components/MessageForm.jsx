f785c7d37b7e867a1f8a637fbab2c11c70e8e0c8
lottiger <linda.lottiger@gmail.com>
2024-04-26 13:29:43 +0200

    l

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -15,7 +15,7 @@ export const MessageForm = () => {
       <label for="email" className='mt-5'>Email:</label>
       <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='rounded bg-stone-900 p-1 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }} />
       <label for="message" >Message:</label>
-      <textarea  name="message" id='message' value={message} rows={8} cols={24} onChange={(e) => setMessage(e.target.value)} required className='rounded bg-stone-900 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }}/>
+      <textarea type="text" name="message" id='message' value={message} rows={8} cols={24} onChange={(e) => setMessage(e.target.value)} required className='rounded bg-stone-900 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }}/>
       <button type="submit" value="Submit" className='bg-yellow-800 text-stone-200 rounded mt-3 py-1 px-6 mb-5 hover:bg-yellow-700 transition-colors'>Send</button>
     </form>
   );
