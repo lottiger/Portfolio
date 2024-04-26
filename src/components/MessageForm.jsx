596e5b9b256b1223ab1530cb5bf68d2d6e0ba263
lottiger <linda.lottiger@gmail.com>
2024-04-26 10:56:24 +0200

    m

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -11,12 +11,11 @@ export const MessageForm = () => {
   };
 
   return (
-    <form name="message" method="POST" data-netlify="true"onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
-      <input type="hidden" name="form-name" value="message" />
+    <form name="contact" method="POST" data-netlify="true"onSubmit={handleSubmit} className='flex flex-col justify-center items-center mt-10 text-stone-200 h-auto w-full border-t border-stone-500'>
       <label className='mt-5'>Email:</label>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='rounded bg-stone-900 p-1 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }} />
+      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='rounded bg-stone-900 p-1 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }} />
       <label>Message:</label>
-      <textarea id='message' value={message} rows={8} cols={24} onChange={(e) => setMessage(e.target.value)} required className='rounded bg-stone-900 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }}/>
+      <textarea  name="message" id='message' value={message} rows={8} cols={24} onChange={(e) => setMessage(e.target.value)} required className='rounded bg-stone-900 mt-1 text-center focus:outline-none focus:ring-1 focus:ring-stone-500' style={{ width: '50%' }}/>
       <button type="submit" value="Submit" className='bg-yellow-800 text-stone-200 rounded mt-3 py-1 px-6 mb-5 hover:bg-yellow-700 transition-colors'>Send</button>
     </form>
   );
