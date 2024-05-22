36b292a80d8923be40fcc33396b7931114bdd17f
lottiger <linda.lottiger@student.kyh.se>
2024-05-22 09:53:07 +0200

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
